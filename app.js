3adfb8fd87940d29b98ebb884c1b66de23a79561
joe4k <joekozhaya@yahoo.com>
2014-08-21 16:32:19 -0400

    Added usermodel service

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ var favicon = require('static-favicon'),
     express = require('express'),
     http = require('http'),
     path = require('path'),
+    hogan = require('hogan-express'),
     brewerydb = require('./service/brewerydb'),
-    qaapi = require('./service/watson/qaapi');
+    qaapi = require('./service/watson/qaapi'),
+    um = require('./service/usermodel/um');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
+// JK added html engine
+app.set('view engine', 'html');
+app.engine('html', hogan);
+app.set('views',__dirname + '/public');
+// end JK
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -21,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/breweries', brewerydb.breweries);
 app.post('/question', qaapi.question);
+app.post('/analyzeText', um.analyzeText);
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
