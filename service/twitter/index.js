5747087dfd41d95cbc1921bb39820ae55ea107fb
Carmine DiMascio <dimascio@us.ibm.com>
2014-08-21 16:43:49 -0400

    twitter intermediate change

---
+++ service/twitter/index.js
@@ -0,0 +1,18 @@
+// PLEASE DO NOT USE THESE CREDENTIALS:
+// These credentials belong to dimascio@us.ibm.com
+var twitter = require('twitter')
+    , twit = new twitter({
+        consumer_key : '644DiRaFxOG7V6Z1COMXEPhhu',
+        consumer_secret : 'VVbaZhCSjp2avkAneXyfjdzKp3vT94lUoJgcWUUOKL4OyjZD3e',
+        access_token_key : '24015584-8sGwoinpk1cVcqSpyoRZdzb5ecJCSofWk122pTBfH',
+        access_token_secret : 'RJKhxSJy5HfRLaqDbrQ3gvIvrQ0ElpkgEeMKrSvzmNPf7'
+    });
+
+exports.tweets = function(req, res) {
+    var text = req.body.query;
+    twit.search(text, req.body.params, function(data) {
+        res.json({
+            tweets : data.statuses
+        });
+    });
+};
