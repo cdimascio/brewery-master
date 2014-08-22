56f99e5a0d5711cd1d6bc7e3b0432d1bd703c683
Carmine DiMascio <dimascio@us.ibm.com>
2014-08-21 22:19:26 -0400

    Sort brewery list alphanumerically Update buttons

--- public/app/js/controller/ctrl.brewery.list.js
+++ public/app/js/controller/ctrl.brewery.list.js
@@ -1,7 +1,15 @@
 define(function () {
     return ['$scope', '$rootScope', '$http', 'BreweryService', function ($scope, $rootScope, $http, blService) {
         $scope.$on('BreweryService.breweries', function () {
-            $scope.data = blService.items() && blService.items().data;
+            var data = blService.items() && blService.items().data;
+            $scope.data = data.sort(function(a, b) {
+                if (a.brewery.name < b.brewery.name) {
+                    return -1;
+                } else if (a.brewery.name > b.brewery.name) {
+                    return 1;
+                }
+                return 0;
+            });
         });
 
         $scope.notifySelect = function (event, brewery) {
