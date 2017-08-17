e1e283dc47c790511049b7a608ef84044e815ee2
Carmine DiMascio <dimascio@us.ibm.com>
2017-08-17 14:36:34 -0400

    set utils

--- service/util.js
+++ service/util.js
@@ -6,15 +6,15 @@
 // These credentials belong to dimascio@us.ibm.com
 exports.twitter = function() {
     return {
-        consumer_key: 'XXX',
-        consumer_secret: 'XXX',
-        access_token_key: 'XXX',
-        access_token_secret: 'XXX'
+        consumer_key: 'gmCqlTcqSPCYZQjK1lvVpQz5m', //'644DiRaFxOG7V6Z1COMXEPhhu',
+        consumer_secret: 'TiLOhNjF1rsH3dygYGsx3GmMJAGRfJmI31s94yEYo1X6LWQOLx', //'VVbaZhCSjp2avkAneXyfjdzKp3vT94lUoJgcWUUOKL4OyjZD3e',
+        access_token_key: '24015584-OMQDV2Cxgw16eau67s96FpIfNd93NWE3u5HfTweUF',//'24015584-8sGwoinpk1cVcqSpyoRZdzb5ecJCSofWk122pTBfH',
+        access_token_secret: 'BoVh2meGtqJxkE90tWS79aB2ukPaDME13KeFi6gqkhaeW'//'RJKhxSJy5HfRLaqDbrQ3gvIvrQ0ElpkgEeMKrSvzmNPf7'
     }
 }
 
 // PLEASE DO NOT USE THESE CREDENTIALS:
 // These credentials belong to dimascio@us.ibm.com
 exports.brewerydb = function() {
-    return 'key=XXX';
+    return 'key=b56430f428d50a7685f43c54b84c87a5';
 }
